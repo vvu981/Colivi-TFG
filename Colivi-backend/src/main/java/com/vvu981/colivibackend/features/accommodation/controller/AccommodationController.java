89d1f0958dcd2bd162fd8fdd0251065dc843ba8a
Victor Vallejo <vvu981@inlumine.ual.es>
2026-06-29 20:43:13 +0200

    fix(accommodations): hard delete mapped

--- Colivi-backend/src/main/java/com/vvu981/colivibackend/features/accommodation/controller/AccommodationController.java
+++ Colivi-backend/src/main/java/com/vvu981/colivibackend/features/accommodation/controller/AccommodationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,7 +69,7 @@ public class AccommodationController {
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
-    @PutMapping("/hardDelete/{id}")
+    @DeleteMapping("/hardDelete/{id}")
     public ResponseEntity<Void> hardDelete(@PathVariable("id") UUID accommodationId,
             @AuthenticationPrincipal User currentUser) {
 
