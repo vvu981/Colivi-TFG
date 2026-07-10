7535ef1ce8b42a641d0f871ee4c75afc93363767
Victor Vallejo <vvu981@inlumine.ual.es>
2026-07-10 13:47:10 +0200

    refactor(jwuAuthenticationFilter): remove unused imports

--- Colivi-backend/src/main/java/com/vvu981/colivibackend/core/security/JwtAuthenticationFilter.java
+++ Colivi-backend/src/main/java/com/vvu981/colivibackend/core/security/JwtAuthenticationFilter.java
@@ -16,10 +16,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Optional;
 
 /**
