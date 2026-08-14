438e7bd76b064564ba10366ec96af428502e7a7f
Victor Vallejo <vvu981@inlumine.ual.es>
2026-08-14 13:18:54 +0200

    fix: vite config test (ignore IDE error)

--- Colivi-frontend/vite.config.ts
+++ Colivi-frontend/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -23,4 +23,4 @@ export default defineConfig({
     globals: true,
     setupFiles: './src/setupTests.ts',
   },
-})
+});
